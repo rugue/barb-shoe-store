03e3d26be132320aba0f74c4503e4a03d86d107f
Osarugue Enehizena <nehirugue@gmail.com>
2024-08-08 02:39:01 +0100

    created dtos for notification

--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,1 +1,19 @@
-export class CreateNotificationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateNotificationDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  readonly title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  readonly message: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly type?: string; // Optional field
+}
